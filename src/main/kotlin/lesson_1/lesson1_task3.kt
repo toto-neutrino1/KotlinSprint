42df61fed64e1f23c76a61e343266422f0acd392
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-09 20:34:06 +0300

    KS-1-2 creating and printing 3 variables for online store

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,2 +1,12 @@
 package lesson_1
 
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("Взлёт: \n$year год \n$hour ч \n$minute мин \n")
+
+    hour = 10
+    minute = 55
+    println("Посадка: \n$hour:$minute")
+}
