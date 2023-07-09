18d02990a7d680e9f210e612b71a65e539214b2e
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-10 00:10:17 +0300

    KS-2-2 salary calculating and printing

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,4 +1,19 @@
 package lesson_2
 
-class lesson2_task2 {
+fun main() {
+    val numOfConstantWorkers = 50
+    val salaryOfConstantWorker = 30000
+
+    val constantCosts = numOfConstantWorkers * salaryOfConstantWorker
+
+    val numOfInterns = 30
+    val salaryOfIntern = 20000
+    val internCosts = numOfInterns * salaryOfIntern
+
+    val allCosts = constantCosts + internCosts
+
+    val meanSalary = allCosts / (numOfConstantWorkers + numOfInterns)
+
+    println("$constantCosts \n$allCosts \n$meanSalary")
+
 }
