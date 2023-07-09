be5d492f3afe6375b0856536503164ab012e72e2
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-09 23:23:43 +0300

    KS-1-5 transforming seconds to minutes and printing time in ordinary min:sec format

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,2 +1,19 @@
 package lesson_1
 
+fun main() {
+    val allSeconds = 6480
+
+    val minutes = if (allSeconds / 60 > 9) {
+        (allSeconds / 60).toString()
+    } else {
+        "0" + (allSeconds / 60).toString()
+    }
+
+    val seconds = if (allSeconds % 60 > 9) {
+        (allSeconds % 60).toString()
+    } else {
+        "0" + (allSeconds % 60).toString()
+    }
+
+    println("$minutes:$seconds")
+}
