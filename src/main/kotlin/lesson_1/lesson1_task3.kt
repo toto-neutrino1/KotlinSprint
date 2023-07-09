0776dc0a010f5c543f19e8f02f5e30243dc11c75
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-09 22:46:37 +0300

    KS-1-3 transform all Int variables to String, renaming commit

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,12 +1,12 @@
 package lesson_1
 
 fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
     println("Взлёт: \n$year год \n$hour ч \n$minute мин \n")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("Посадка: \n$hour:$minute")
 }
