1be499483cb9a924105dd52a46915702332adfb5
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-09 23:49:22 +0300

    KS-2-1 calculating mean grade and printing with 2 digits

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,12 +1,2 @@
 package lesson_2
 
-fun main() {
-    val grade1 = 3
-    val grade2 = 4
-    val grade3 = 3
-    val grade4 = 5
-
-    val meanGrade = (grade1 + grade2 + grade3 + grade4) / 4.0
-
-    println("%.2f".format(meanGrade))
-}
