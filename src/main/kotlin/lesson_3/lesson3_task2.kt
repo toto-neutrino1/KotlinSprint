c0e6b5b34b68b26a325fc26dc988a4b9b652bfdd
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-10 15:45:49 +0300

    KS-3-2 output personal data

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,2 +1,23 @@
 package lesson_3
 
+fun main() {
+
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+    output(surname, name, patronymic, age)
+
+    age = 22
+    surname = "Сидорова"
+    output(surname, name, patronymic, age)
+}
+
+fun output(surname: String,  name: String,  patronymic: String, age: Int) {
+    val yearEnding = when {
+        age % 10 in 2..4 -> "года"
+        age % 10 == 1 && age % 100 != 11 -> "год"
+        else -> "лет"
+    }
+    println("$surname $name $patronymic, $age $yearEnding")
+}
