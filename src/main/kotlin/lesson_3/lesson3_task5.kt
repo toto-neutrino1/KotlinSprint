236a2c91825e8476717fa5199ef3bf3c33ddbfb2
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-10 16:30:18 +0300

    KS-3-5 string parsing

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,2 +1,14 @@
 package lesson_3
 
+fun main() {
+    val outputString = "D2-D4;0"
+
+    val indexOfDash = outputString.indexOf('-')
+    //val indexOfSemicolon = outputString.indexOf(';')
+
+    val begin = outputString.take(2)
+    val end = outputString.slice(indexOfDash + 1..indexOfDash + 2)
+    val moveNumber = outputString.substringAfter(';')
+
+    println("$begin \n$end \n$moveNumber")
+}
