082929a5b51a0f7aa5270058b5f064e80da009a7
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-11 14:45:49 +0300

    KS-4-1 print reservation availability

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,2 +1,18 @@
 package lesson_4
 
+const val TABLES = 13
+fun main() {
+    val reservedToday = 13
+    val reservedTomorrow = 9
+
+    //printTableAvailability("сегодня", reservedToday)
+    //printTableAvailability("завтра", reservedTomorrow)
+
+    val reservationToday = "Доступность столиков на сегодня: ${reservedToday < TABLES}"
+    val reservationTomorrow = "Доступность столиков на завтра: ${reservedTomorrow < TABLES}"
+    println("$reservationToday \n$reservationTomorrow")
+}
+
+fun printTableAvailability(date: String, reservedTable: Int) {
+    println("Доступность столиков на $date: ${reservedTable < TABLES}")
+}
