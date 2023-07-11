dbd0510170afa35f5df7e33ad0dba5da00d17adc
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-11 17:35:48 +0300

    KS-4-3 print are good conditions for plant growth

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,2 +1,16 @@
 package lesson_4
 
+const val GOOD_WEATHER = "солнечная"
+const val AWNING = "открыт"
+const val HUMIDITY = 20
+const val BAD_SEASON = "зима"
+fun main() {
+    val isGoodWeather = "солнечная" == GOOD_WEATHER // по условию тип boolean должен быть
+    val isGoodAwning = "открыт" == AWNING // по условию тип boolean должен быть
+    val humidity = 20
+    val season = "зима"
+
+    val areGoodConditions = isGoodWeather && isGoodAwning && (humidity == HUMIDITY) && (season != BAD_SEASON)
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $areGoodConditions")
+}
