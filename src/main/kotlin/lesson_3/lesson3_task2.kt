c22b7f65031b8babc54ea804275fe04fc4987ae1
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-11 13:48:58 +0300

    KS-3-2 output personal data: rename output function

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -6,14 +6,14 @@ fun main() {
     val name = "Татьяна"
     val patronymic = "Сергеевна"
     var age = 20
-    output(surname, name, patronymic, age)
+    printUserData(surname, name, patronymic, age)
 
     age = 22
     surname = "Сидорова"
-    output(surname, name, patronymic, age)
+    printUserData(surname, name, patronymic, age)
 }
 
-fun output(surname: String,  name: String,  patronymic: String, age: Int) {
+fun printUserData(surname: String, name: String, patronymic: String, age: Int) {
     val yearEnding = when {
         age % 10 in 2..4 -> "года"
         age % 10 == 1 && age % 100 != 11 -> "год"
