26172614d52e344e008dd3c115168b6dec1d52ee
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-14 17:24:04 +0300

    KS-5-3 lottery: guess 2 numbers

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,2 +1,27 @@
 package lesson_5
 
+const val NUMBER_1 = 17
+const val NUMBER_2 = 98
+
+fun main() {
+    println("Угадайте 2 числа от 1 до 100\n")
+    println("Введите, чему равно первое число в поле ниже:")
+    val num1 = readln().toInt()
+
+    println("Введите, чему равно второе число в поле ниже:")
+    val num2 = readln().toInt()
+
+    val areGuessedBothNumbers =
+        (num1 == NUMBER_1 && num2 == NUMBER_2) || (num1 == NUMBER_2 && num2 == NUMBER_1)
+
+    val isGuessedOneNumber =
+        (num1 == NUMBER_1 || num1 == NUMBER_2) || (num2 == NUMBER_1 || num2 == NUMBER_2)
+
+    println(
+        if (areGuessedBothNumbers) "Поздравляем! Вы выиграли главный приз!"
+        else if (isGuessedOneNumber) "Вы выиграли утешительный приз!"
+        else "Неудача! Крутите барабан!"
+    )
+
+    println("\nДля победы нужно было ввести числа $NUMBER_1 и $NUMBER_2")
+}
