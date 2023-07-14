15bd35e0f8416db3f5a0b41da19388b94ebafc7e
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-14 18:07:55 +0300

    KS-5-5 lottery: guess 2 numbers, random numbers every time

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,2 +1,30 @@
 package lesson_5
 
+import kotlin.random.Random
+
+fun main() {
+
+    val number1 = Random.nextInt(1, 101)
+    val number2 = Random.nextInt(1, 101)
+
+    println("Угадайте 2 числа от 1 до 100\n")
+    println("Введите, чему равно первое число в поле ниже:")
+    val num1 = readln().toInt()
+
+    println("Введите, чему равно второе число в поле ниже:")
+    val num2 = readln().toInt()
+
+    val areGuessedBothNumbers =
+        (num1 == number1 && num2 == number2) || (num1 == number2 && num2 == number1)
+
+    val isGuessedOneNumber =
+        (num1 == number1 || num1 == number2) || (num2 == number1 || num2 == number2)
+
+    println(
+        if (areGuessedBothNumbers) "Поздравляем! Вы выиграли главный приз!"
+        else if (isGuessedOneNumber) "Вы выиграли утешительный приз!"
+        else "Неудача! Крутите барабан!"
+    )
+
+    println("\nДля победы нужно было ввести числа $number1 и $number2")
+}
