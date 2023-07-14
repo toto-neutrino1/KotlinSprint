86b5526bb355c18bd192b39ce1ff904a36388faa
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-14 16:45:00 +0300

    KS-5-2 input year of birth, calculate and check user age

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,2 +1,16 @@
 package lesson_5
 
+import java.util.*
+
+const val AGE_OF_MAJORITY = 18
+fun main() {
+
+    println("Введите ваш год рождения:")
+    val yearOfBirth = readln().toInt()
+
+    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+    val userAge = currentYear - yearOfBirth
+
+    if (userAge >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
+}
