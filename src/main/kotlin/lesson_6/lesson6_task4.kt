df138613c01d7ebec82454dd4c75d81e61273919
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-17 15:56:13 +0300

    KS-6-4 guess number (5 attempts)

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -1,2 +1,31 @@
 package lesson_6
 
+fun main() {
+    val guessedNumber = (1..9).random()
+    var numOfAttempts = 5
+
+    while (numOfAttempts > 0) {
+        println("Введите число от 1 до 9 в поле ниже:")
+        if (readln().toInt() == guessedNumber) {
+            println("Это была великолепная игра!")
+            return
+        }
+        else {
+            printErrorMessage(numOfAttempts)
+            numOfAttempts--
+        }
+    }
+
+    println("Было загадано число: $guessedNumber")
+}
+
+fun printErrorMessage(numOfAttempts: Int) {
+    val str1 = "Неверно. Осталось ${numOfAttempts - 1}"
+    val str2 = when (numOfAttempts - 1) {
+        in 2..4 -> "попытки"
+        0 -> "попыток"
+        else -> "попытка"
+    }
+
+    println("$str1 $str2\n")
+}
