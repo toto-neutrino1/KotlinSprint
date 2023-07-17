a91b94f47930e61cec38ec721d7e3fc81d84e287
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-17 15:23:53 +0300

    KS-6-2 easy timer creation

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,2 +1,8 @@
 package lesson_6
 
+fun main() {
+    println("Введите сколько секунд нужно засечь:")
+    val seconds = readln().toInt()
+    Thread.sleep(1000L * seconds)
+    println("\nПрошло $seconds секунд")
+}
