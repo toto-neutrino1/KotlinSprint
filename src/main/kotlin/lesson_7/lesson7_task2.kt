c2c1583b52de7a789cad2117355aec9e1e6049f0
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-18 15:28:33 +0300

    KS-7-2 random sms code generator

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -1,2 +1,25 @@
 package lesson_7
 
+fun main() {
+    val numbers = 1000..9999
+    var code: Int
+    var userInput: Int
+
+    var counter = 0
+
+    while (counter < 100) {
+        code = numbers.random()
+        println("Ваш код авторизации: $code")
+
+        println("Введите код авторизации:")
+        userInput = readln().toInt()
+        println()
+
+        if (userInput == code) {
+            println("Добро пожаловать!")
+            return
+        }
+
+        counter++
+    }
+}
