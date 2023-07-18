d0d579ac29e047d0ee94667d0ee1a5c67b6a8ebc
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-18 16:07:50 +0300

    KS-7-5 random password generator (user sets length)

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,2 +1,26 @@
 package lesson_7
 
+fun main() {
+    println("Пароль какой длины вы хотите сгенерировать? Введите число:")
+    val passwordLen = readln().toInt()
+
+    val password = generatePassword(passwordLen)
+    println("\nПароль:\n$password")
+}
+
+fun generatePassword(passwordLen: Int): String {
+    val digits = 0..9
+    val lowerLetters = 'a'..'z'
+    val upperLetters = 'A'..'Z'
+
+    val password: MutableList<String> = mutableListOf()
+
+    for (i in 1..passwordLen) {
+        if ((1..2).random() == 1) password.add("${digits.random()}")
+        else {
+            if ((1..2).random() == 1) password.add("${lowerLetters.random()}")
+            else password.add("${upperLetters.random()}")
+        }
+    }
+    return password.joinToString(separator = "")
+}
