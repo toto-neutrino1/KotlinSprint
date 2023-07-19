465d9e8dddbed14fec450277687fc77b42267661
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-19 15:56:35 +0300

    KS-8-2 search of ingredient in array

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -1,2 +1,17 @@
 package lesson_8
 
+fun main() {
+    val ingredients = arrayOf("соль", "сахар", "мука", "яйцо", "ванилин", "яблоко")
+
+    println("Какой ингредиент вы хотите найти? \nВведите название в единственном числе:")
+    val userIngredient = readln()
+
+    for (elem in ingredients) {
+        if (userIngredient == elem) {
+            println("\nИнгредиент \"$elem\" в рецепте есть")
+            return
+        }
+    }
+
+    println("\nТакого ингредиента в рецепте нет")
+}
