093de66bf7ca21a5d671c60502a930beb0fe6f4f
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-20 16:21:38 +0300

    KS-9-4 user's ingredients sorting and output

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -1,2 +1,9 @@
 package lesson_9
 
+fun main() {
+    println("Введите в поле ниже через запятую 5 ингредиентов:")
+    val ingredients = readln().split(",").toMutableList()
+    ingredients.sort()
+    println("\nВаши ингредиенты:")
+    ingredients.forEach { println(it) }
+}
