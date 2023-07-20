238c0e0526927748459fa056f7de1eb4cdbb8395
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-20 16:39:32 +0300

    KS-9-5 user's unique ingredients sorting and output

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,2 +1,11 @@
 package lesson_9
 
+fun main() {
+    println("Введите ниже 5 ингредиентов, каждый ингредиент нужно ввести с новой строки:")
+    val ingredients = MutableList(5) { readln() }.toSet().toMutableList()
+    ingredients.sort()
+    println("\nВаши ингредиенты:")
+    println((ingredients.joinToString(separator = ", ", postfix = ".")).replaceFirst(
+        ingredients[0].first(), ingredients[0].first().uppercaseChar())
+    )
+}
