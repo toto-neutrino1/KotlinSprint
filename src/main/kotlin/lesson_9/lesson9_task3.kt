4822b7af21d287b62b6b777fdac6a48fe912352b
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-20 16:12:01 +0300

    KS-9-3 number of ingredients per n portions calculating

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -1,2 +1,15 @@
 package lesson_9
 
+fun main() {
+    val numOfComponentsPerOnePortion = listOf(2, 50, 15)
+    println("Сколько порций омлета вы хотите приготовить?")
+    val numOfPortions = readln().toInt()
+
+    val neededComponents = numOfComponentsPerOnePortion.map { it * numOfPortions }
+    println(
+        "\nНа $numOfPortions порций вам понадобится: " +
+                "Яиц – ${neededComponents[0]} шт, " +
+                "молока – ${neededComponents[1]} мл, " +
+                "сливочного масла – ${neededComponents[2]} гр."
+    )
+}
