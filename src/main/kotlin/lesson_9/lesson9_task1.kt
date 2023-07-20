6c1bf4be6cd350a2f8570d06f380b772604af7f1
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-20 15:52:55 +0300

    KS-9-1 list of ingredients output

--- src/main/kotlin/lesson_9/lesson9_task1.kt
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -1,2 +1,7 @@
 package lesson_9
 
+fun main() {
+    val ingredients = listOf("соль", "сахар", "шоколад", "мука", "яйцо")
+    println("В рецепте есть следующие ингредиенты:")
+    ingredients.forEach { println(it) }
+}
