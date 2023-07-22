80f74d25dfa081dfe01d0ce5edfd0b9f67014604
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-22 13:32:12 +0300

    KS-9-4 delete MutableList, add sorted() method

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -2,8 +2,7 @@ package lesson_9
 
 fun main() {
     println("Введите в поле ниже через запятую 5 ингредиентов:")
-    val ingredients = readln().split(",").toMutableList()
-    ingredients.sort()
+    val ingredients = readln().split(",").sorted()
     println("\nВаши ингредиенты:")
     ingredients.forEach { println(it) }
 }
