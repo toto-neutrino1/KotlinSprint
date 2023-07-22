e2cbd125cb66871aca8ba8c95395c0561507b237
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-22 18:42:18 +0300

    KS-10-2 validation of login/password length

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,2 +1,16 @@
 package lesson_10
 
+fun main() {
+    println("Введите логин:")
+    val login = readln()
+
+    println("Введите пароль:")
+    val password = readln()
+
+    println(
+        if (checkUserData(login) && checkUserData(password)) "\nДобро пожаловать!"
+        else "\nЛогин или пароль недостаточно длинные"
+    )
+}
+
+fun checkUserData(userData: String) = userData.length >= 4
