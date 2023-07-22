85f72c991151286e75a6a5dd35e5d82825cba5ba
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-22 14:17:52 +0300

    KS-9-5 MutableList -> List, add sorted() method

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,8 +2,7 @@ package lesson_9
 
 fun main() {
     println("Введите ниже 5 ингредиентов, каждый ингредиент нужно ввести с новой строки:")
-    val ingredients = MutableList(5) { readln() }.toSet().toMutableList()
-    ingredients.sort()
+    val ingredients = List(5) { readln() }.toSet().toList().sorted()
     println("\nВаши ингредиенты:")
     println((ingredients.joinToString(separator = ", ", postfix = ".")).replaceFirst(
         ingredients[0].first(), ingredients[0].first().uppercaseChar())
