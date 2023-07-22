b38623d57038ebfcff49a0d04a0cd1a6de00e19a
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-22 19:35:02 +0300

    KS-10-4 game of dice, many cycles

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -1,2 +1,30 @@
 package lesson_10
 
+fun main() {
+   var userWins = 0
+    if (isUserWins()) userWins++
+
+    println("Хотите бросить кости еще раз? Введите Да или Нет:")
+    var userAnswer = readln()
+
+    while (userAnswer.equals("да", ignoreCase = true)) {
+        if (isUserWins()) userWins++
+
+        println("\nХотите бросить кости еще раз? Введите Да или Нет:")
+        userAnswer = readln()
+    }
+
+    println("\nПользователь выиграл $userWins партий")
+}
+
+fun rollDice() = List(2) { (1..6).random() }
+
+fun isUserWins(): Boolean {
+    val userRoll = rollDice()
+    val compRoll = rollDice()
+
+    println("Компьютер выбросил: ${compRoll.joinToString()}")
+    println("Пользователь выбросил: ${userRoll.joinToString()}\n")
+
+    return compRoll.sum() < userRoll.sum()
+}
