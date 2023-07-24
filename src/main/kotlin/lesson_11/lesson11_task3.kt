d35016ec19f5f0a1aca8726f0962ca440709ecf6
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-24 17:23:51 +0300

    KS-11-3 Room class creation: easy voice social network

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -1,2 +1,70 @@
 package lesson_11
 
+fun main() {
+    val room1 = Room(
+        cover = "Мяч",
+        name = "Спорт",
+        members = listOf(
+            Member("@user1"),
+            Member("@user2", "разговаривает"),
+            Member("@user3", "пользователь заглушен"),
+            Member("@user4"),
+        )
+    )
+
+    room1.printRoomInfo()
+    room1.members[0].longClick()
+
+    val room2 = Room(
+        cover = "Ноутбук",
+        name = "IT",
+        members = listOf(
+            Member("@user5"),
+            Member("@user6"),
+            Member("@user7"),
+        )
+    )
+    room2.printRoomInfo()
+    room2.members[1].sayStatus()
+    room2.members[2].muteUser()
+    room2.printRoomInfo()
+    room2.members[1].turnOffMicrophone()
+    room2.printRoomInfo()
+}
+
+private class Room(
+    val cover: String,
+    val name: String,
+    val members: List<Member>,
+) {
+    fun printRoomInfo() {
+        println("Room \"$name\" info: \ncover = $cover")
+        for (member in members) {
+            member.printMemberData()
+        }
+        println()
+    }
+}
+
+private class Member(
+    val nickName: String,
+    private var status: String = "микрофон выключен"
+) {
+
+    fun longClick() = println("$nickName\n")
+    fun printMemberData() = println("$nickName - $status")
+
+    fun sayStatus() {
+        status = "разговаривает"
+    }
+
+
+    fun turnOffMicrophone() {
+        status = "микрофон выключен"
+    }
+
+
+    fun muteUser() {
+        status = "пользователь заглушен"
+    }
+}
