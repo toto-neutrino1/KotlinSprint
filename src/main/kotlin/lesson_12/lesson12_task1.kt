80243ae7173aea400207bc50ba0ad195e34fa2f1
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-25 14:16:53 +0300

    KS-12-1 WeatherTracker class

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -1,2 +1,31 @@
 package lesson_12
 
+fun main() {
+    val weather1 = WeatherTracker()
+    weather1.dayTemperature = 40
+    weather1.nightTemperature = 28
+    weather1.wasItRaining = false
+    weather1.atmPressure = 0.98
+
+    val weather2 = WeatherTracker()
+    weather2.dayTemperature = 30
+    weather2.nightTemperature = 20
+    weather2.wasItRaining = true
+    weather2.atmPressure = 1.1
+
+    weather1.printWeatherData()
+    weather2.printWeatherData()
+}
+
+
+class WeatherTracker {
+    var dayTemperature = 25
+    var nightTemperature = 15
+    var wasItRaining = false
+    var atmPressure = 1.0
+
+    fun printWeatherData() {
+        println("Day temp.: $dayTemperature C \nNight temp.: $nightTemperature C")
+        println("Was it raining: $wasItRaining \nAtm. pressure: $atmPressure atm.\n")
+    }
+}
