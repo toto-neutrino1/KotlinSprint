e7ff31199e936fe790c5cf6f13ad083f883dbc6c
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-26 15:04:03 +0300

    KS-13-4 Phonebook class: add and print user input contacts

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -1,2 +1,57 @@
 package lesson_13
 
+fun main() {
+    val phonebook = Phonebook()
+    phonebook.addContacts()
+    phonebook.printAllContacts()
+}
+
+class Phonebook(
+    private val contacts: MutableList<Contact4> = mutableListOf()
+) {
+    fun addContacts() {
+        addContact()
+        println("\nЕсли хотите добавить новую запись, введите \\“да\\”")
+        var answer = readln().lowercase()
+
+        while (answer == "да") {
+            addContact()
+            println("\nЕсли хотите добавить новую запись, введите \\“да\\”")
+            answer = readln().lowercase()
+        }
+    }
+
+    private fun addContact() {
+        println("\nВведите имя:")
+        var name = readln()
+        if (name.isEmpty()) name = "null"
+
+        println("\nВведите номер телефона:")
+        val number = readln().toLongOrNull()
+
+        println("\nВведите компанию:")
+        var company = readln()
+        if (company.isEmpty()) company = "null"
+
+        if (number != null) {
+            contacts.add(
+                Contact4(name = name, phoneNumber = number, company = company)
+            )
+        }
+    }
+
+    fun printAllContacts() = contacts.forEach { it.printContact() }
+}
+
+class Contact4(
+    val name: String?,
+    val phoneNumber: Long?,
+    val company: String?,
+) {
+    fun printContact() {
+        val unknown = "не указано"
+        println(
+            "\nИмя: ${name ?: unknown} \nНомер: ${phoneNumber ?: unknown} \nКомпания: ${company ?: unknown}"
+        )
+    }
+}
