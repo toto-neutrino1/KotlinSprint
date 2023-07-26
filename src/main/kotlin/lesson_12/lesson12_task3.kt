4339b8cd1ca92c05de168617992e328658a3acb1
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-27 00:14:48 +0300

    KS-12-3 class renaming

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,16 +1,16 @@
 package lesson_12
 
 fun main() {
-    val weather1 = WeatherTracker2(40, 28, atmPressure = 0.98)
-    val weather2 = WeatherTracker2(30, 20, true, 1.1)
-    val weather3 = WeatherTracker2(5, -5, atmPressure = 1.0)
+    val weather1 = WeatherTracker3(40, 28, atmPressure = 0.98)
+    val weather2 = WeatherTracker3(30, 20, true, 1.1)
+    val weather3 = WeatherTracker3(5, -5, atmPressure = 1.0)
 
     weather1.printWeatherData()
     weather2.printWeatherData()
     weather3.printWeatherData()
 }
 
-class WeatherTracker2(
+class WeatherTracker3(
     var dayTemperature: Int,
     var nightTemperature: Int,
     var wasItRaining: Boolean = false,
