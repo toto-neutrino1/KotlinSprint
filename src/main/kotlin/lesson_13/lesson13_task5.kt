129d088c0f472f1043b04250e8a135fffc79ed8e
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-26 15:56:45 +0300

    KS-13-5 wrong input, exception handling

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -1,3 +1,14 @@
 package lesson_13
 
+import java.lang.NumberFormatException
 
+
+fun main() {
+    println("Введите номер телефона:")
+    try {
+        val number = readln().toLong()
+        println("\nВы ввели номер: $number")
+    } catch (e: NumberFormatException) {
+        println("\nНомер может содержать только цифры")
+    }
+}
