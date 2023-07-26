1c5adab6e62576ffb23df0e15022ed473be169c1
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-27 00:06:00 +0300

    KS-13-2 renaming class

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -1,11 +1,11 @@
 package lesson_13
 
 fun main() {
-    val contact = Contact(name = "Ростислав", phoneNumber = 89123456789, company = null)
+    val contact = Contact2(name = "Ростислав", phoneNumber = 89123456789, company = null)
     contact.printContact()
 }
 
-private class Contact(
+private class Contact2(
     val name: String,
     val phoneNumber: Long,
     val company: String?,
