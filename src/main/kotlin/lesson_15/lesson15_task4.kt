b5067a9beb545df9edc19108084c94fdc15bbbee
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-28 20:16:25 +0300

    KS-15-4 Music shop goods classes/interface

--- src/main/kotlin/lesson_15/lesson15_task4.kt
+++ src/main/kotlin/lesson_15/lesson15_task4.kt
@@ -1,2 +1,32 @@
 package lesson_15
 
+fun main() {
+    val guitar = MusicalInstrument("Гитара", 10)
+    val guitarAccessories1 = Accessories("струны", 100, guitar)
+    guitar.search()
+}
+
+class MusicalInstrument(
+    override val name: String,
+    override val numOfUnitsInStock: Int
+    ) : Good(), Search {
+    override fun search() {
+        super.search()
+        print(" комплектующих к \"$name\"\n")
+    }
+}
+
+class Accessories(
+    override val name: String,
+    override val numOfUnitsInStock: Int,
+    val musicalInstrument: MusicalInstrument
+) : Good()
+
+abstract class Good {
+    abstract val name: String
+    abstract val numOfUnitsInStock: Int
+}
+
+interface Search {
+    fun search() = print("Выполняется поиск")
+}
