a4a0c28c57870a1c783f9f52ad69d14e4128699a
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-28 14:18:16 +0300

    KS-15-1 Flying/Swimming interfaces creating

--- src/main/kotlin/lesson_15/lesson15_task1.kt
+++ src/main/kotlin/lesson_15/lesson15_task1.kt
@@ -1,2 +1,46 @@
 package lesson_15
 
+fun main() {
+    val carp = CrucianCarp()
+    val seagull = SeagullBird()
+    val duck = Duck()
+
+    carp.canSwim()
+    seagull.canFly()
+    duck.canFly()
+    duck.canSwim()
+}
+
+class CrucianCarp() : Swimming {
+    override fun canSwim() {
+        print("Карась ")
+        super.canSwim()
+    }
+}
+
+class SeagullBird() : Flying {
+    override fun canFly() {
+        print("Чайка ")
+        super.canFly()
+    }
+}
+
+class Duck() : Flying, Swimming {
+    override fun canFly() {
+        print("Утка ")
+        super.canFly()
+    }
+
+    override fun canSwim() {
+        print("Утка ")
+        super.canSwim()
+    }
+}
+
+interface Flying {
+    fun canFly() = println("умеет летать")
+}
+
+interface Swimming {
+    fun canSwim() = println("умеет плавать")
+}
