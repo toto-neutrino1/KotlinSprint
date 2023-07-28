6b067f30cee0b607814788b8884630cb38d04b2f
toto-neutrino1 <tolyan7007@gmail.com>
2023-07-28 18:30:15 +0300

    KS-15-3 Admin/User/Forum classes: write/read/delete msg

--- src/main/kotlin/lesson_15/lesson15_task3.kt
+++ src/main/kotlin/lesson_15/lesson15_task3.kt
@@ -1,2 +1,54 @@
 package lesson_15
 
+fun main() {
+    val admins = MutableList(3) { Admin("admin${it + 1}") }
+    val users = MutableList(5) { User("user${it + 1}") }
+
+    val forum = Forum(admins, users)
+    forum.admins[2].writeMessage("Добро пожаловать на форум!", forum.messages)
+    forum.users[1].writeMessage("Привет!", forum.messages)
+    forum.users[0].writeMessage("Какие планы?", forum.messages)
+    forum.admins[1].writeMessage("Учимся!", forum.messages)
+    forum.readMessages()
+
+    forum.admins[0].deleteMessage("user2: Привет!", forum.messages)
+    forum.readMessages()
+    forum.printUsers()
+
+    forum.admins[2].deleteUser("user1", forum.users)
+    forum.printUsers()
+    forum.printAdmins()
+}
+
+class Forum(
+    val admins: MutableList<Admin>,
+    val users: MutableList<User> = mutableListOf(),
+    val messages: MutableList<String> = mutableListOf()
+) {
+    fun readMessages() = admins[0].readMessages(messages)
+
+    fun printUsers() = println("\nUsers: ${users.joinToString(separator = ", ") { it.user }}")
+
+    fun printAdmins() = println("Admins: ${admins.joinToString(separator = ", ") { it.user }}")
+}
+
+class Admin(admin: String) : User(admin) {
+    fun deleteMessage(message: String, messages: MutableList<String>) {
+        messages.remove(message)
+    }
+
+    fun deleteUser(user: String, users: MutableList<User>) {
+        users.removeIf { it.user == user }
+    }
+}
+
+open class User(val user: String) : BasicUser() {
+    override fun writeMessage(message: String, messages: MutableList<String>) {
+        messages.add("$user: $message")
+    }
+}
+
+abstract class BasicUser {
+    fun readMessages(messages: MutableList<String>) = println(messages.joinToString(separator = "\n", postfix = "\n"))
+    abstract fun writeMessage(message: String, messages: MutableList<String>)
+}
