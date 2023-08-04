84cddd76881e0d8119190e5ff4deb5c29a06e63d
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-04 14:47:18 +0300

    KS-16-1 Dice class, encapsulation

--- src/main/kotlin/lesson_16/lesson16_task1.kt
+++ src/main/kotlin/lesson_16/lesson16_task1.kt
@@ -1,2 +1,11 @@
 package lesson_16
 
+fun main() {
+    val dice = Dice()
+    dice.printDiceNumber()
+}
+
+class Dice {
+    private val number = (1..6).random()
+    fun printDiceNumber() = println("На кубике выпало число $number")
+}
