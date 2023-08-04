04e0a2808ab46c7aa4b895fd297e7f2df1db5e2e
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-05 00:39:19 +0300

    KS-17-5 User class: login/password; set()/get()

--- src/main/kotlin/lesson_17/lesson17_task5.kt
+++ src/main/kotlin/lesson_17/lesson17_task5.kt
@@ -1,2 +1,28 @@
 package lesson_17
 
+fun main() {
+    val user = User(login = "user1", password = "qwerty")
+    println("Логин: ${user.login}, пароль: ${user.password}\n")
+    user.login = "user1"
+    user.login = "user2"
+    user.password = "12345"
+}
+
+class User(
+    login: String,
+    password: String
+) {
+    var login: String = login
+        set(value) {
+            if (field != value) {
+                field = value
+                println("Логин изменён. Новый логин: $field\n")
+            } else println("Новый и старый логин не могут совпадать. Логин: $field\n")
+        }
+
+    var password: String = password
+        get() = "*".repeat(field.length)
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+}
