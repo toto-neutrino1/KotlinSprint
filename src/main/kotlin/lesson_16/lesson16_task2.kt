3fe0aade1d84a1622b59fc5e1fa495d36b5f26f5
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-04 15:01:42 +0300

    KS-16-2 Circle class: private field, calculate S/L methods

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -1,2 +1,17 @@
 package lesson_16
 
+import java.util.*
+import kotlin.math.pow
+
+private const val PI = 3.14
+
+fun main() {
+    val circle = Circle(2.22)
+    println("Длина окружности: ${circle.getCircumference()}")
+    println("Площадь круга: ${circle.getCircleArea()}")
+}
+
+class Circle(private val radius: Double) {
+    fun getCircumference() = "%.2f".format(locale = Locale.US, 2 * PI * radius)
+    fun getCircleArea() = "%.2f".format(locale = Locale.US, PI * radius.pow(2))
+}
