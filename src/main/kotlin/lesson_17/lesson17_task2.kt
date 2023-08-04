0a7bb3f5561538177fe1b71af79dc82fa46b9030
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-04 16:39:27 +0300

    KS-17-2 Ship class: setter (prohibit on changing var)

--- src/main/kotlin/lesson_17/lesson17_task2.kt
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -1,2 +1,18 @@
 package lesson_17
 
+fun main() {
+    val ship = Ship(name = "Титаник", averageSpeed = 23, homePort = "Ливерпуль" )
+    println("Корабль \"${ship.name}\" \nсредняя скорость: ${ship.averageSpeed} \nпорт приписки: ${ship.homePort}\n")
+    ship.name = "Millennium Falcon"
+}
+
+class Ship(
+    name: String,
+    val averageSpeed: Int,
+    val homePort: String
+) {
+    var name: String = name
+        set(value) {
+            println("Имя корабля изменять нельзя!")
+        }
+}
