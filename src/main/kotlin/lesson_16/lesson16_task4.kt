077cacf80ce24e0996caf4980b5d5b29a457b7d9
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-04 15:24:29 +0300

    KS-16-4 Order class: id/status fields, changeStatus method

--- src/main/kotlin/lesson_16/lesson16_task4.kt
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -1,2 +1,17 @@
 package lesson_16
 
+fun main() {
+    val order = Order(1)
+    order.changeStatus("В пути")
+    order.changeStatus("Доставлен")
+}
+
+class Order(
+    private val id: Int,
+    private var status: String = "Обрабатывается"
+) {
+    fun changeStatus(newStatus: String) {
+        println("Статус \"$status\" был изменен на статус \"$newStatus\"\n")
+        status = newStatus
+    }
+}
