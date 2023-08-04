2141a7cedd48c2edf386a26a847b6daf17a3be60
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-04 16:22:57 +0300

    KS-17-1 Quiz class: getter/setter

--- src/main/kotlin/lesson_17/lesson17_task1.kt
+++ src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -1,2 +1,22 @@
 package lesson_17
 
+fun main() {
+    val quiz = Quiz()
+    println("${quiz.question} \n${quiz.answer}\n")
+    quiz.question = "Какой КПД у дизельного двигателя?"
+    quiz.answer = "33 %"
+    println("${quiz.question} \n${quiz.answer}\n")
+}
+
+class Quiz {
+    var question: String = "Вопрос: "
+        set(value) {
+            field = "Вопрос: $value"
+        }
+
+    var answer: String = ""
+        get() = "Верный ответ: $field"
+        set(value) {
+            field = value
+        }
+}
