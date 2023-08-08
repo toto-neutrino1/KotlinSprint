be68f0a4aa6a1ed86288434e4ae4962429b89960
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-08 14:39:58 +0300

    KS-18-3 Animals, play(), sleep(), eat() methods

--- src/main/kotlin/lesson_18/lesson18_task3.kt
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -1,2 +1,32 @@
 package lesson_18
 
+fun main() {
+    val fox = Fox()
+    val dog = Dog()
+    val cat = Cat()
+
+    val animals = listOf(fox, dog, cat)
+    printEatForAllAnimals(animals)
+}
+
+fun printEatForAllAnimals(animals: List<Animal>) = animals.forEach { it.eat() }
+
+open class Animal(
+    val name: String
+) {
+    fun play() = println("$name - играет")
+    fun sleep() = println("$name - спит")
+    open fun eat() {}
+}
+
+class Fox() : Animal(name = "лиса") {
+    override fun eat() = println("$name - ест ягоды")
+}
+
+class Dog() : Animal(name = "собака") {
+    override fun eat() = println("$name - ест кости")
+}
+
+class Cat() : Animal(name = "кошка") {
+    override fun eat() = println("$name - ест рыбу")
+}
