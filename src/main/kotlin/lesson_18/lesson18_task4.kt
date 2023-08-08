8987d24f98be44aabe62f0614a50434a899dffe7
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-08 15:58:53 +0300

    KS-18-4 different Boxes, printSurfaceArea method

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -1,2 +1,39 @@
 package lesson_18
 
+import java.util.*
+import kotlin.math.pow
+
+fun main() {
+    val rectangularBox = RectangularBox(id = 1, length = 1.0, width = 2.0, height = 3.0)
+    val cubicBox = CubicBox(id = 2, length = 2.5)
+
+    printAreaOfAllBoxes(listOf(rectangularBox, cubicBox))
+}
+
+fun printAreaOfAllBoxes(boxes: List<Box>) = boxes.forEach { it.printSurfaceArea() }
+
+abstract class Box(val id: Int) {
+    abstract fun printSurfaceArea()
+}
+
+class RectangularBox(
+    id: Int,
+    val length: Double,
+    val width: Double,
+    val height: Double
+) : Box(id) {
+    override fun printSurfaceArea() {
+        val area = "%.2f".format(locale = Locale.US,2 * (length * (width + height) + width * height))
+        println("$id. Прямоугольная коробка площадью $area")
+    }
+}
+
+class CubicBox(
+    id: Int,
+    val length: Double,
+) : Box(id) {
+    override fun printSurfaceArea() {
+        val area = "%.2f".format(locale = Locale.US,6 * length.pow(2))
+        println("$id. Кубическая коробка площадью $area")
+    }
+}
