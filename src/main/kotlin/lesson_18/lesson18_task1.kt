5a6f9eb8b9f2f6ad2f884b22bac9aa9d43d23064
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-08 13:34:35 +0300

    KS-18-1 print goods info, polymorphism

--- src/main/kotlin/lesson_18/lesson18_task1.kt
+++ src/main/kotlin/lesson_18/lesson18_task1.kt
@@ -1,2 +1,30 @@
 package lesson_18
 
+fun main() {
+    val order1 = OrderWithOneGood(id = 1, good = "хлеб")
+    val order2 = OrderWithManyGoods(id = 2, goods = listOf("кефир", "молоко", "соль", "сахар"))
+
+    printAllOrdersInfo(listOf(order1, order2))
+}
+
+fun printAllOrdersInfo(orders: List<Order>) = orders.forEach { it.printOrderInfo() }
+
+open class Order(
+    val id: Int
+) {
+    open fun printOrderInfo() {}
+}
+
+class OrderWithOneGood(
+    id: Int,
+    val good: String
+) : Order(id) {
+    override fun printOrderInfo() = println("$id. Заказан товар: $good\n")
+}
+
+class OrderWithManyGoods(
+    id: Int,
+    val goods: List<String>
+) : Order(id) {
+    override fun printOrderInfo() = println("$id. Заказаны товары: ${goods.joinToString()}\n")
+}
