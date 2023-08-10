68e1247a38f36af805d654abf29b5e83c1f0162c
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-10 17:07:15 +0300

    KS-19-1 print Fish enum elements

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,2 +1,14 @@
 package lesson_19
 
+fun main() {
+    printAllFishes()
+}
+
+fun printAllFishes() = println("Вы можете добавить в аквариум рыб: \n${Fish.values().joinToString()}")
+
+enum class Fish {
+    GUPPY,
+    ANGELFISH,
+    GOLDFISH,
+    SIAMESE_FIGHTING_FISH
+}
