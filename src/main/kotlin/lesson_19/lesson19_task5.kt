3828ee3adbbe80ee5c3c798dbfd4f4aca7427789
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-10 23:54:27 +0300

    KS-19-5 FileCard creation (humans name / sex data)

--- src/main/kotlin/lesson_19/lesson19_task5.kt
+++ src/main/kotlin/lesson_19/lesson19_task5.kt
@@ -1,2 +1,42 @@
 package lesson_19
 
+fun main() {
+    val fileCard = FileCard()
+    repeat(5) {
+        fileCard.addHumanToFileCard()
+    }
+
+    fileCard.printFileCardData()
+}
+
+class FileCard(
+    private val humans: MutableList<Human> = mutableListOf()
+) {
+    init {
+        println("Формат ввода данных \nСначада введите имя человека, а затем через пробел -  его/её пол")
+        println("Пол: м - \"мужской\", ж - \"женский\" (вводить можно в любом регистре на русскоязычной раскладке)\n")
+    }
+
+    fun addHumanToFileCard() {
+        println("Введите имя и пол (м/ж):")
+        val humanData = readln().split(" ")
+
+        val sex = if (humanData[1].lowercase() == "м") Sex.MALE else Sex.FEMALE
+
+        humans.add(Human(humanData[0], sex))
+    }
+
+    fun printFileCardData() {
+        humans.forEach { println("${it.name} - ${it.sex}") }
+    }
+}
+
+enum class Sex {
+    MALE,
+    FEMALE
+}
+
+class Human(
+    val name: String,
+    val sex: Sex
+)
