9f96fc6f89be1db427618b54c652fdfc70937c82
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-11 19:44:52 +0300

    KS-20-2 Player class, takeHealingPotion lambda

--- src/main/kotlin/lesson_20/lesson20_task2.kt
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -1,3 +1,17 @@
 package lesson_20
 
+fun main() {
+    val player = Player(name = "Raiden", currentHealth = 50)
+    player.printPlayerInfo()
+    player.takeHealingPotion()
+    player.printPlayerInfo()
+}
 
+class Player(
+    val name: String,
+    var currentHealth: Int,
+    val maxHealth: Int = 100
+) {
+    val takeHealingPotion = { currentHealth = maxHealth }
+    val printPlayerInfo = { println("Игрок $name, уровень здоровья: $currentHealth") }
+}
