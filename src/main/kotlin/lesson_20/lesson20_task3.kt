2be76c7dc2b09056dec1a37c7f9fafbb86b10af2
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-11 19:52:14 +0300

    KS-20-3 Player class, checkKey lambda

--- src/main/kotlin/lesson_20/lesson20_task3.kt
+++ src/main/kotlin/lesson_20/lesson20_task3.kt
@@ -1,2 +1,16 @@
 package lesson_20
 
+fun main() {
+    val player3 = Player3(name = "John")
+    player3.checkKey()
+
+    player3.hasKey = true
+    player3.checkKey()
+}
+
+class Player3(
+    val name: String,
+    var hasKey: Boolean = false
+) {
+    val checkKey = { if (hasKey) println("Игрок $name открыл дверь") else println("Дверь заперта") }
+}
