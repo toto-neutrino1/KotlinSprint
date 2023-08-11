0d9f44f9fee95716aa42614ee979e797131406fb
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-11 21:05:27 +0300

    KS-20-4 Lambdas List

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -4,7 +4,6 @@ fun main() {
     val strings = List(10) { "Строка ${it + 1}" }
     val lambdas = strings.map { { println("Нажат элемент $it") } }
 
-
     for (i in 1..lambdas.size step 2) {
         lambdas[i]()
     }
